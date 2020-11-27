c5f22b40b02f992e68a7a82b1e35fd3724ac08d4
iamelimars <elicodes@DESKTOP-LHNC09S.localdomain>
2020-11-27 18:56:27 -0500

    Fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-      {/* <Nav /> */}
       <Navbar />
       <Hero />
     </>
